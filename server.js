933216e2a51bdb667221c8dc67828580bd60a988
Shady Coder <shadysaleh01@gmail.com>
2020-09-12 12:51:44 -0700

    Update server.js

--- server.js
+++ server.js
@@ -7,56 +7,54 @@ const app = express()
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const server = http.createServer(handleRequest);
-server.listen(PORT, function () {
-   console.log("App listening on PORT " + PORT);
-});
+// const server = http.createServer(handleRequest);
+// server.listen(PORT, function () {
+//    console.log("App listening on PORT " + PORT);
+// });
+
+app.get("/", (req, res) => {
+   res.sendFile(__dirname + "/home.html")
+
+})
+app.get("/tables", (req, res) => {
+   res.sendFile(__dirname + "/tables.html")
+})
+
+app.get("/reserve", (req, res) => {
+   res.sendFile(__dirname + "/reserve.html")
+})
+
+app.listen(PORT, () => { console.log("App listening on PORT " + PORT) })
+// function handleRequest(request, response) {
+//    const path = request.url;
+
+//    $("#view").on("click", function () {
+//       fs.readFile(__dirname + "/tables.html", (err, data) => {
+//          if (err) throw err;
+
+//          res.writeHead(200, { "Content-Type": "text/html" });
+//          res.end(data);
+//       });
+//    })
+
+//    $("#reservation").on("click", function () {
+//       fs.readFile(__dirname + "/reserve.html", (err, data) => {
+//          if (err) throw err;
+
+//          res.writeHead(200, { "Content-Type": "text/html" });
+//          res.end(data);
+//       });
+//    })
+//    $("#home").on("click", function () {
+//       fs.readFile(__dirname + "/home.html", (err, data) => {
+//          if (err) throw err;
+
+//          res.writeHead(200, { "Content-Type": "text/html" });
+//          res.end(data);
+//       });
+//    })
 
 
-function handleRequest(request, response) {
-   const path = request.url;
-
-   $("#view").on("click", function () {
-      fs.readFile(__dirname + "/tables.html", (err, data) => {
-         if (err) throw err;
-
-         res.writeHead(200, { "Content-Type": "text/html" });
-         res.end(data);
-      });
-   })
-
-   $("#reservation").on("click", function () {
-      fs.readFile(__dirname + "/reserve.html", (err, data) => {
-         if (err) throw err;
-
-         res.writeHead(200, { "Content-Type": "text/html" });
-         res.end(data);
-      });
-   })
-   $("#home").on("click", function () {
-      fs.readFile(__dirname + "/home.html", (err, data) => {
-         if (err) throw err;
-
-         res.writeHead(200, { "Content-Type": "text/html" });
-         res.end(data);
-      });
-   })
-
-
-   // switch (path) {
-   //    case "/":
-
-   //       break;
-   //    case "/reserve":
-   //       someHtml("reserve", response);
-   //       break;
-   //    case "/tables":
-   //       someHtml("tables", response);
-   //       break;
-   //    default:
-   //       notFound(request.url, request, response);
-   // }
-}
 
 
 // function someHtml(fileName, res) 
